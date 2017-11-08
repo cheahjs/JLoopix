a2ec2957e81e8702ed9e338692743c596d8ae56b
Jun Siang Cheah <cheahjunsiang@gmail.com>
2017-11-08 15:19:56 +0000

    Increase mix node count to 6
    Each layer needs at least 2 nodes to calculate paths correctly.

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Should be in sync with generate_keys.sh
-MIXNODE_COUNT=3
+MIXNODE_COUNT=6
 PROVIDER_COUNT=1
 CLIENT_COUNT=2
 
@@ -17,39 +17,45 @@ fi
 
 for ((i=1;i<=PROVIDER_COUNT;i++)); do
     echo "Starting provider $i"
+    $DOCKER_PATH rm -f "provider_$i" > /dev/null 2>&1
     $DOCKER_PATH run \
     --net=host \
     -v "$DIR/build/example.db:/loopix/loopix/example.db" \
     -v "$DIR/build/loopix_keys/provider_$i/publicProvider.bin:/loopix/loopix/publicProvider.bin" \
     -v "$DIR/build/loopix_keys/provider_$i/secretProvider.prv:/loopix/loopix/secretProvider.prv" \
     -w "/loopix/loopix" \
-    -d \
+    -dit \
+    --name="provider_$i" \
     deathmax/loopix \
-    twistd -ny run_provider.py
+    twistd --nodaemon --python=run_provider.py
 done
 
 for ((i=1;i<=MIXNODE_COUNT;i++)); do
     echo "Starting mixnode $i"
+    $DOCKER_PATH rm -f "mix_$i" > /dev/null 2>&1
     $DOCKER_PATH run \
     --net=host \
     -v "$DIR/build/example.db:/loopix/loopix/example.db" \
     -v "$DIR/build/loopix_keys/mixnode_$i/publicMixnode.bin:/loopix/loopix/publicMixnode.bin" \
     -v "$DIR/build/loopix_keys/mixnode_$i/secretMixnode.prv:/loopix/loopix/secretMixnode.prv" \
     -w "/loopix/loopix" \
-    -d \
+    -dit \
+    --name="mix_$i" \
     deathmax/loopix \
-    twistd -ny run_mixnode.py
+    twistd --nodaemon --python=run_mixnode.py
 done
 
 for ((i=1;i<=CLIENT_COUNT;i++)); do
     echo "Starting client $i"
+    $DOCKER_PATH rm -f "client_$i" > /dev/null 2>&1
     $DOCKER_PATH run \
     --net=host \
     -v "$DIR/build/example.db:/loopix/loopix/example.db" \
     -v "$DIR/build/loopix_keys/client_$i/publicClient.bin:/loopix/loopix/publicClient.bin" \
     -v "$DIR/build/loopix_keys/client_$i/secretClient.prv:/loopix/loopix/secretClient.prv" \
     -w "/loopix/loopix" \
-    -d \
+    -dit \
+    --name="client_$i" \
     deathmax/loopix \
-    twistd -ny run_client.py
+    twistd --nodaemon --python=run_client.py
 done
