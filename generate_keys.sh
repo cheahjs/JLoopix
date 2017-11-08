9fc82ba5fc764ef4c0289aab8c18a85c35cfb72e
Jun Siang Cheah <cheahjunsiang@gmail.com>
2017-11-08 13:58:08 +0000

    Clean build folder before generating new keys

--- generate_keys.sh
+++ generate_keys.sh
@@ -5,12 +5,16 @@ PROVIDER_COUNT=1
 CLIENT_COUNT=2
 CWD=$(pwd)
 
+HOST=127.0.0.1
+
+rm -R build
+
 # Generate keys
 for ((i=1;i<=PROVIDER_COUNT;i++)); do
     printf "Creating provider $i\n"
     mkdir -p "build/loopix_keys/provider_$i/"
     (cd "build/loopix_keys/provider_$i/" && \
-        python "$CWD/external/loopix/loopix/setup_provider.py" $((7000 + $i)) "localhost" "provider_$i")
+        python "$CWD/external/loopix/loopix/setup_provider.py" $((30000 + $i)) $HOST "provider_$i")
 done
 
 for ((i=1;i<=CLIENT_COUNT;i++)); do
@@ -18,15 +22,15 @@ for ((i=1;i<=CLIENT_COUNT;i++)); do
     printf "Creating client $i with provider $CLIENT_PROVIDER\n"
     mkdir -p "build/loopix_keys/client_$i/"
     (cd "build/loopix_keys/client_$i/" && \
-        python "$CWD/external/loopix/loopix/setup_client.py" $((8000 + $i)) "localhost" "client_$i" "provider_$CLIENT_PROVIDER")
+        python "$CWD/external/loopix/loopix/setup_client.py" $((31000 + $i)) $HOST "client_$i" "provider_$CLIENT_PROVIDER")
 done
 
-for ((i=1;i<=CLIENT_COUNT;i++)); do
-    MIX_LAYER=$(($i % 3))
+for ((i=1;i<=MIXNODE_COUNT;i++)); do
+    MIX_LAYER=$((($i-1) % 3))
     printf "Creating mixnode $i in layer $MIX_LAYER\n"
     mkdir -p "build/loopix_keys/mixnode_$i/"
     (cd "build/loopix_keys/mixnode_$i/" && \
-        python "$CWD/external/loopix/loopix/setup_mixnode.py" $((9000 + $i)) "localhost" "mix_$i" "$MIX_LAYER")
+        python "$CWD/external/loopix/loopix/setup_mixnode.py" $((32000 + $i)) $HOST "mix_$i" "$MIX_LAYER")
 done
 
 # Add nodes to DB
