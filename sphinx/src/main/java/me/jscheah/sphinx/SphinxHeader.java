abb384ea6ce677c5f314f8c6b3aaf4bdb6f48771
Jun Siang Cheah <cheahjunsiang@gmail.com>
2018-01-20 11:25:36 +0000

    Add helper method to SphinxHeader to generate msgpack Values

--- sphinx/src/main/java/me/jscheah/sphinx/SphinxHeader.java
+++ sphinx/src/main/java/me/jscheah/sphinx/SphinxHeader.java
@@ -1,6 +1,13 @@
 package me.jscheah.sphinx;
 
+import me.jscheah.sphinx.msgpack.Packer;
 import org.bouncycastle.math.ec.ECPoint;
+import org.msgpack.value.Value;
+import org.msgpack.value.impl.ImmutableArrayValueImpl;
+import org.msgpack.value.impl.ImmutableBinaryValueImpl;
+import org.msgpack.value.impl.ImmutableExtensionValueImpl;
+
+import java.io.IOException;
 
 public class SphinxHeader {
     public ECPoint alpha;
@@ -11,5 +18,17 @@ public class SphinxHeader {
         this.alpha = alpha;
         this.beta = beta;
         this.gamma = gamma;
+    }
+
+    public Value toValue() {
+        try {
+            return new ImmutableArrayValueImpl(new Value[] {
+                    new ImmutableExtensionValueImpl((byte) 2, Packer.ecPointToByteArray(alpha)),
+                    new ImmutableBinaryValueImpl(beta),
+                    new ImmutableBinaryValueImpl(gamma)
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
