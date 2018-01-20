2c55465e4f702cc0c95d692efa6cffa661206bc4
Jun Siang Cheah <cheahjunsiang@gmail.com>
2018-01-20 11:33:36 +0000

    Implement core methods

--- jloopix/src/main/java/me/jscheah/jloopix/Core.java
+++ jloopix/src/main/java/me/jscheah/jloopix/Core.java
@@ -1,4 +1,26 @@
 package me.jscheah.jloopix;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import static java.util.stream.Collectors.groupingBy;
+
 public class Core {
+    private static SecureRandom random = new SecureRandom();
+    public static byte[] generateRandomBytes(int length) {
+        byte[] out = new byte[length];
+        random.nextBytes(out);
+        return out;
+    }
+
+    /***
+     * Returns a list of groups of mixes based on ascending order of groupID
+     * @param mixes List of mixes to use
+     * @return List of groups of mixes
+     */
+    public static List<List<MixNode>> groupLayeredTopology(List<MixNode> mixes) {
+        return new ArrayList<>(new TreeMap<>(mixes.stream().collect(groupingBy(x -> x.groupID))).values());
+    }
 }
