dbc212e3dd77237f5eb6a79279201686d870e779
Jun Siang Cheah <cheahjunsiang@gmail.com>
2018-01-25 15:25:42 +0000

    Remove unused docker build scripts

--- docker_build.sh
+++ docker_build.sh
@@ -7,24 +7,6 @@ else
     DOCKER_PATH="docker"
 fi
 
-# # Build client image
-# $DOCKER_PATH build \
-#     -t deathmax/loopix-client \
-#     -f docker/loopix_client/Dockerfile \
-#     .
-
-# # Build provider image
-# $DOCKER_PATH build \
-#     -t deathmax/loopix-provider \
-#     -f docker/loopix_provider/Dockerfile \
-#     .
-
-# # Build mixnode image
-# $DOCKER_PATH build \
-#     -t deathmax/loopix-mixnode \
-#     -f docker/loopix_mixnode/Dockerfile \
-#     .
-
 # Build single Loopix image
 $DOCKER_PATH build \
     -t deathmax/loopix \
