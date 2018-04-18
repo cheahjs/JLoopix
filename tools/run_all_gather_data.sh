94cbc18021df56f91ee6f6d018bb8b149ec458c4
Jun Siang Cheah <me@jscheah.me>
2018-04-18 22:41:23 +0100

    Set final parameters for data gathering

--- tools/run_all_gather_data.sh
+++ tools/run_all_gather_data.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-# #############################
-# # Bandwidth data collection #
-# #############################
+#############################
+# Bandwidth data collection #
+#############################
 
-# # Rate parameters
-# RATE_START=0.1
-# RATE_END=0.1
-# RATE_TICK=0.1
-# DELAY=0.001
+# Rate parameters
+RATE_START=0.1
+RATE_END=3.0
+RATE_TICK=0.1
+DELAY=0.001
 
-# # Network parameters
-# JAVA_COUNT=2
-# PYTHON_COUNT=4
-# MIX_COUNT=6
-# PROVIDER_COUNT=2
+# Network parameters
+JAVA_COUNT=250
+PYTHON_COUNT=250
+MIX_COUNT=6
+PROVIDER_COUNT=4
 
-# # Initial setup for creating keys
-# python setup_network.py \
-#     --mix $MIX_COUNT --provider $PROVIDER_COUNT --client $PYTHON_COUNT --client-java $JAVA_COUNT
-# ./generate_keys.sh
+# Initial setup for creating keys
+python setup_network.py \
+    --mix $MIX_COUNT --provider $PROVIDER_COUNT --client $PYTHON_COUNT --client-java $JAVA_COUNT
+./generate_keys.sh
 
-# # Collect bandwidth data
-# for rate in $(seq $RATE_START $RATE_TICK $RATE_END); do
-#     echo "Running at rate $rate"
-#     # Setup network config
-#     python setup_network.py \
-#         --rate-real $rate --rate-drop $rate --rate-loop $rate --delay $DELAY \
-#         --mix $MIX_COUNT --provider $PROVIDER_COUNT --client $PYTHON_COUNT --client-java $JAVA_COUNT
-#     # Run docker containers
-#     ./docker_run.sh
-#     # Gather bandwidth data
-#     ./gather_bandwidth_data.sh
-#     # Stop containers
-#     ./docker_stop.sh
-# done
+# Collect bandwidth data
+for rate in $(seq $RATE_START $RATE_TICK $RATE_END); do
+    echo "Running at rate $rate"
+    # Setup network config
+    python setup_network.py \
+        --rate-real $rate --rate-drop $rate --rate-loop $rate --delay $DELAY \
+        --mix $MIX_COUNT --provider $PROVIDER_COUNT --client $PYTHON_COUNT --client-java $JAVA_COUNT
+    # Run docker containers
+    ./docker_run.sh
+    # Gather bandwidth data
+    ./gather_bandwidth_data.sh
+    # Stop containers
+    ./docker_stop.sh
+done
 
 ###########################
 # Latency data collection #
@@ -43,8 +43,8 @@
 PROVIDER_COUNT=4
 MIX_COUNT=6
 CLIENT_START=5
-CLIENT_END=5
-CLIENT_TICK=20
+CLIENT_END=250
+CLIENT_TICK=35
 
 # Collect latency data
 for count in $(seq $CLIENT_START $CLIENT_TICK $CLIENT_END); do
